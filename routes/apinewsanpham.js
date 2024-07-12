0bfc7647b64474fd6314f4c1a7546337dd63e1cb
trafdual <traz08102003@gmail.com>
2024-07-12 11:51:13 +0700

    f

--- routes/apinewsanpham.js
+++ routes/apinewsanpham.js
@@ -940,9 +940,9 @@ router.post('/editblog/:idblog', async(req, res) => {
                 if (img[index]) {
                     nd.img = img[index];
                 }
-                if (tieude[index]) {
-                    nd.tieude = tieude[index];
-                }
+
+                nd.tieude = tieude[index];
+
             });
 
             for (let i = blog.noidung.length; i < content.length; i++) {
