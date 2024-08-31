9a726bcc3de232757a4ad772a465811afaa57c35
trafdual <traz08102003@gmail.com>
2024-08-31 14:13:43 +0700

    g

---
+++ ecosystem.config.js
@@ -0,0 +1,12 @@
+module.exports={
+    apps:[
+        {
+            name:'DOC-BAO',
+            script:'./index.js',
+            env:{
+                NODE_ENV:'production',
+                PORT:1001
+            }
+        }
+    ]
+}
